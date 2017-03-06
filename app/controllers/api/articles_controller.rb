2dae5888af9e301c62d1940ea0733b725f7e2117
adamshen <adam_ruby@126.com>
2017-03-06 11:59:02 +0800

    support delete article

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -1,26 +1,22 @@
 class Api::ArticlesController < ApplicationController
   before_action :authenticate_api_user!, only: :create
+  before_action :find_article, only: [:show, :update, :destroy]
+  before_action :check_params, only: [:create, :update]
 
   def create
-    requires! :article_topic_id, :title, :content
-
     article = topic.articles.create!(article_params)
     render json: { id: article.id }
   end
 
   def show
-    article = Article.find(params[:id])
-    render json: article
+    render json: @article
   end
 
   def update
-    requires! :article_topic_id, :title, :content
+    @article.article_topic = topic
+    @article.update(article_params)
 
-    article = Article.find(params[:id])
-    article.article_topic = topic
-    article.update(article_params)
-
-    render json: { id: article.id }
+    render json: { id: @article.id }
   end
 
   def index
@@ -29,7 +25,20 @@ class Api::ArticlesController < ApplicationController
     render json: articles
   end
 
+  def destroy
+    @article.destroy
+    render json: { success: true }
+  end
+
   private
+  def find_article
+    @article = Article.find(params[:id])
+  end
+
+  def check_params
+    requires! :article_topic_id, :title, :content
+  end
+
   def topic
     ArticleTopic.find(params[:article_topic_id])
   end
